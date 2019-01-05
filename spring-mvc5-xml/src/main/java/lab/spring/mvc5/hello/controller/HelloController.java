9824d64303421b4387576acbfc8f927b0aa615dc
tribles <senthilkumar.s.shanmugam@oracle.com>
2019-01-05 15:38:57 +0530

    added

--- spring-mvc5-xml/src/main/java/lab/spring/mvc5/hello/controller/HelloController.java
+++ spring-mvc5-xml/src/main/java/lab/spring/mvc5/hello/controller/HelloController.java
@@ -2,12 +2,16 @@ package lab.spring.mvc5.hello.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 	
+	@RequestMapping(value="hello",method = RequestMethod.GET)
 	public String sayHello(@RequestParam(value="name",required=false,defaultValue="Hello") String name,Model model) {
+		System.out.println("inside the controller");
 		model.addAttribute("name",name);
 		return "hello";
 	}
