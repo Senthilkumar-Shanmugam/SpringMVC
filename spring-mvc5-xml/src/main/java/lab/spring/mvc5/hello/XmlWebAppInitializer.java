801fec7f1b93477877d2c4a0959885e484baf91d
tribles <senthilkumar.s.shanmugam@oracle.com>
2019-01-05 16:48:08 +0530

    changes to deploy spring mvc app with out web.xml

---
+++ spring-mvc5-xml/src/main/java/lab/spring/mvc5/hello/XmlWebAppInitializer.java
@@ -0,0 +1,30 @@
+package lab.spring.mvc5.hello;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class XmlWebAppInitializer implements WebApplicationInitializer{
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		XmlWebApplicationContext rootContext = new XmlWebApplicationContext();
+		rootContext.setConfigLocation("/WEB-INF/app-config.xml");
+		servletContext.addListener(new ContextLoaderListener(rootContext));
+		
+		XmlWebApplicationContext webContext = new XmlWebApplicationContext();
+		webContext.setConfigLocation("/WEB-INF/web-config.xml");
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", 
+        		         new DispatcherServlet(webContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+		
+		
+	}
+
+}
